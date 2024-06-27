0a10c7df9a9d92b240da966c1034bc3faccaeb66
sachin29aug <sachin.sachdeva@gmail.com>
2024-06-27 19:53:03 +0530

    data set parser - in progress

--- app/controllers/SystemAdmin.java
+++ app/controllers/SystemAdmin.java
@@ -10,62 +10,37 @@ import play.mvc.Result;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class SystemAdmin extends Controller {
     public Result importBooks() throws IOException {
         StringBuilder sb = new StringBuilder();
         int count = 0;
-        List<String> filePaths = Arrays.asList(
-                "datasets/Personal Development/self-help.html",
-                "datasets/Personal Development/productivity.html",
-                "datasets/Personal Development/communication-skills.html",
-                "datasets/Personal Development/creativity.html",
-                "datasets/Personal Development/education.html",
-                "datasets/Personal Development/biography.html",
-                "datasets/Personal Development/philosophy.html",
+        Map<String, List<String>> categoriesMap = new LinkedHashMap<>();
+        categoriesMap.put("Personal Development", Arrays.asList("self-help", "productivity", "communication-skills", "creativity", "education", "biography", "philosophy"));
+        categoriesMap.put("Mind & Spirit", Arrays.asList("psychology", "spirituality", "mindfulness"));
+        categoriesMap.put("Business & Economics", Arrays.asList("business", "economics", "leadership", "entrepreneurship", "marketing"));
+        categoriesMap.put("Family & Lifestyle", Arrays.asList("childrens", "parenting", "travel"));
+        categoriesMap.put("Science & Environment", Arrays.asList("science", "environment", "gardening"));
+        categoriesMap.put("Arts & Humanities", Arrays.asList("art", "design", "architecture", "folklore", "history", "politics", "law"));
 
-                "datasets/Mind & Spirit/psychology.html",
-                "datasets/Mind & Spirit/spirituality.html",
-                "datasets/Mind & Spirit/mindfulness.html",
-
-                "datasets/Business & Economics/business.html",
-                "datasets/Business & Economics/economics.html",
-                "datasets/Business & Economics/leadership.html",
-                "datasets/Business & Economics/entrepreneurship.html",
-                "datasets/Business & Economics/marketing.html",
-
-                "datasets/Family & Lifestyle/childrens.html",
-                "datasets/Family & Lifestyle/parenting.html",
-                "datasets/Family & Lifestyle/travel.html",
-
-                "datasets/Science & Environment/science.html",
-                "datasets/Science & Environment/environment.html",
-                "datasets/Science & Environment/gardening.html",
-
-                "datasets/Arts & Humanities/art.html",
-                "datasets/Arts & Humanities/design.html",
-                "datasets/Arts & Humanities/architecture.html",
-                "datasets/Arts & Humanities/folklore.html",
-                "datasets/Arts & Humanities/history.html",
-                "datasets/Arts & Humanities/politics.html",
-                "datasets/Arts & Humanities/law.html"
-        );
-
-        for(String filePath : filePaths) {
-            List<Book> books = importBooks1(filePath);
-            for(Book book: books) {
-                sb.append(++count + "\n");
-                sb.append(book.title + "\n");
-                sb.append(book.author  + "\n");
-                sb.append(book.avgRating  + "\n");
-                sb.append(book.ratingsCount  + "\n");
-                sb.append(book.publishDate  + "\n");
-                sb.append("<a href='https://www.goodreads.com/" + book.goodReadsLink  + "' target='_blank'>" + book.title + "</a>"  + "\n");
-                sb.append("========================"  + "\n\n");
+        for(Map.Entry<String, List<String>> entry : categoriesMap.entrySet()) {
+            String category = entry.getKey();
+            List<String> subCategories = entry.getValue();
+            for (String subCategory: subCategories) {
+                List<Book> books = importBooks1(category, subCategory);
+                for(Book book: books) {
+                    sb.append(++count + "\n");
+                    sb.append(book.category + "\n");
+                    sb.append(book.subCategory + "\n");
+                    sb.append(book.title + "\n");
+                    sb.append(book.author  + "\n");
+                    sb.append(book.avgRating  + "\n");
+                    sb.append(book.ratingsCount  + "\n");
+                    sb.append(book.publishDate  + "\n");
+                    sb.append("<a href='https://www.goodreads.com/" + book.goodReadsLink  + "' target='_blank'>" + book.title + "</a>"  + "\n");
+                    sb.append("========================"  + "\n\n");
+                }
             }
         }
 
@@ -73,13 +48,14 @@ public class SystemAdmin extends Controller {
     }
 
     public static String getRandomBookLink() throws IOException {
-        List<Book> books = importBooks1("datasets/Personal Development/self-help.html");
+        List<Book> books = importBooks1("Personal Development", "self-help");
         int randomIndex = new Random().nextInt(books.size());
         return books.get(randomIndex).goodReadsLink;
     }
 
-    public static List<Book> importBooks1(String filePath) throws IOException {
+    public static List<Book> importBooks1(String category, String subCategory) throws IOException {
         String confDir = Paths.get("conf").toAbsolutePath().toString();
+        String filePath = "datasets/" + category + "/" + subCategory + ".html";
         File inputFile = new File(confDir, filePath);
         Document doc = Jsoup.parse(inputFile, "UTF-8");
 
@@ -103,8 +79,7 @@ public class SystemAdmin extends Controller {
                     publishDate = parts[2].replace("published", "").trim();
                 }
                 String goodReadsLink = "<a href='https://www.goodreads.com/" + bookElements.get(i).attr("href")  + "' target='_blank'>" + bookTitle + "</a>";
-
-                books.add(new Book(bookTitle, authorName, avgRating, ratingsCount, publishDate, goodReadsLink));
+                books.add(new Book(bookTitle, authorName, avgRating, ratingsCount, publishDate, goodReadsLink, category, subCategory));
             }
         }
         return books;
@@ -118,13 +93,17 @@ class Book {
     public String ratingsCount;
     public String publishDate;
     public String goodReadsLink;
+    public String category;
+    public String subCategory;
 
-    public Book(String title, String author, String avgRating, String ratingsCount, String publishDate, String goodReadsLink) {
+    public Book(String title, String author, String avgRating, String ratingsCount, String publishDate, String goodReadsLink, String category, String subCategory) {
         this.title = title;
         this.author = author;
         this.avgRating = avgRating;
         this.ratingsCount = ratingsCount;
         this.publishDate = publishDate;
         this.goodReadsLink = goodReadsLink;
+        this.category = category;
+        this.subCategory = subCategory;
     }
 }
